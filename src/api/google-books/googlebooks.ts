9e5b33b6c695dcd910b60ebea64bc1df8df2823f
Avellow <avellow@yandex.ru>
2023-03-19 16:02:00 +0300

    featrue: google api & typing

---
+++ src/api/google-books/googlebooks.ts
@@ -0,0 +1,52 @@
+import { booksPerLoading } from "../../utils/constants";
+
+const API_KEY = "AIzaSyAVbG2ZaNggyn6kUSFu04jPbL-Ee0ASSGI";
+const API_URL = "https://www.googleapis.com/books/v1/volumes?";
+
+export enum CategoriesEnum {
+  ALL = "all",
+  ART = "art",
+  BIOGRAPHY = "biography",
+  COMPUTERS = "computers",
+  HISTORY = "history",
+  MEDICAL = "medical",
+  POETRY = "poetry",
+}
+
+export enum SortingEnum {
+  RELEVANCE = "relevance",
+  NEWEST = "newest",
+}
+
+export interface SearchParams {
+  searchString: string;
+  category?: CategoriesEnum;
+  sorting?: SortingEnum;
+  startIndex: number;
+}
+
+function buildQueryParamsString({
+  searchString,
+  category,
+  sorting,
+  startIndex = 0,
+}: SearchParams): string {
+  let result = `q=${searchString}`;
+
+  if (category && category !== "all") {
+    result += `+subject:${category}`;
+  }
+  if (sorting) {
+    result += `&orderBy=${sorting}`;
+  }
+
+  result += `&startIndex=${startIndex}&maxResults=${booksPerLoading}`;
+
+  return result;
+}
+
+export const getBooks = async (params: SearchParams): Promise<Response> => {
+  return await fetch(
+    `${API_URL}${buildQueryParamsString(params)}&key=${API_KEY}`
+  );
+};
---
+++ src/utils/constants.ts
@@ -0,0 +1,1 @@
+export const booksPerLoading = 30;
